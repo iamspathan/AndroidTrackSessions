d705c7ce5977f1c1f3247768b904801516f2d1ab
Sohail Pathan <khan.sohel0786@gmail.com>
2021-02-11 01:10:13 +0530

    first session added

---
+++ AndroidTrack/app/src/main/java/dev/iamspathan/androidtrack/MainActivity.kt
@@ -0,0 +1,84 @@
+package dev.iamspathan.androidtrack
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+
+    private val TAG = "FirstActivityState"
+
+    lateinit var button:Button
+
+    private var isLogin = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.nextButton)
+
+        button.setOnClickListener {
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        Log.d(TAG, "OnCreate")
+    }
+
+
+    fun checkStatus(isLogin:Boolean){
+        if (isLogin){
+            Toast.makeText(this, "User Logged In", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            Toast.makeText(this, "User Logged Out", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "OnStart")
+
+       checkStatus(isLogin)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "OnStop")
+        isLogin = false
+        checkStatus(isLogin)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "OnDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "OnRestart")
+    }
+
+
+
+
+}
